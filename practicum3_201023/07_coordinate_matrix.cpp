08f87c2048b0040827c920e7b70869f1a50cd733
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2023-10-20 13:36:06 +0300

    Add files via upload

---
+++ practicum3_201023/07_coordinate_matrix.cpp
@@ -0,0 +1,16 @@
+/*#include <iostream>
+
+int main() {
+
+	int n = 0, m = 0;
+	std::cin >> n >> m;
+
+	for (int row = 1; row <= n; row++) {
+		for (int col = 1; col <= m; col++) {
+			std::cout << "(" << row << ", " << col << ") ";
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}*/
