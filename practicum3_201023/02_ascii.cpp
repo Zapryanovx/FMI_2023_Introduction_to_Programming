8b4420432ece32ab32357e17724b023e1758be2a
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2023-10-20 12:40:22 +0300

    Add files via upload

---
+++ practicum3_201023/02_ascii.cpp
@@ -0,0 +1,12 @@
+/*#include <iostream>
+
+int main() {
+
+	int start = 0, end = 0;
+	std::cin >> start >> end;
+
+	for (int i = start; i <= end; i++) {
+		std::cout << i << " --> " << char(i) << std::endl;
+	}
+	return 0;
+}*/
