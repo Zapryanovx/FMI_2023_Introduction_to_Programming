a45a7851a3ff42e38a1698b7c7bdde1cfa886fb3
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2023-11-03 13:26:01 +0200

    Add files via upload

---
+++ practicum5_031123/05_ascii.cpp
@@ -0,0 +1,18 @@
+/*#include <iostream>
+
+int main() {
+
+	const int SIZE = 5;
+	char arr[SIZE] = {};
+
+	int startCh = 0;
+	std::cin >> startCh;
+
+	for (int i = 0; i < SIZE; i++) {
+		startCh += i;
+		arr[i] = startCh;
+		std::cout << arr[i] << " ";
+	}
+
+	return 0;
+}*/
