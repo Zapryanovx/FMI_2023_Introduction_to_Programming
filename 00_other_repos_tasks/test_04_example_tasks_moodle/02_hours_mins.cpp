7d4a122f12f47eb34783a31a0249781247127bfb
Ilian Zapryanov <68477629+Zapryanovx@users.noreply.github.com>
2024-01-17 14:25:51 +0200

    Add files via upload

---
+++ 00_other_repos_tasks/test_04_example_tasks_moodle/02_hours_mins.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//
+//void secsToHoursMinsFormat(int secs);
+//
+//int main()
+//{
+//	
+//	int secs = 0;
+//	std::cin >> secs;
+//
+//	secsToHoursMinsFormat(secs);
+//
+//	return 0;
+//}
+//
+//void secsToHoursMinsFormat(int secs)
+//{
+//	int hours = secs / 60 / 60;
+//	int mins = secs / 60 % 60;
+//
+//	std::cout << hours << ":" << mins;
+//}
---
+++ 00_other_repos_tasks/test_04_example_tasks_moodle/05_sum_of_even.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//
+//int getSumOfEven(int nums[], int n);
+//
+//int main()
+//{
+//	int numsOne[1] = { 3 };
+//	int numsTwo[3] = { 3, 5, 9 };
+//	int numsThree[7] = { 3, 21, 914, 123, 3, 3, 0 };
+//	
+//	std::cout<<getSumOfEven(numsOne, 1)<<" ";
+//	std::cout << getSumOfEven(numsTwo, 3) << " ";
+//	std::cout << getSumOfEven(numsThree, 7) << " ";
+//
+//	return 0;
+//}
+//
+//
+//int getSumOfEven(int nums[], int n)
+//{
+//	int sum = 0;
+//	for (int i = 0; i < n; i += 2)
+//	{
+//		sum += nums[i];
+//	}
+//	
+//	return sum;
+//}
